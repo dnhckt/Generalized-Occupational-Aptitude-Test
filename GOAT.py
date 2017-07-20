c45e42e66189b5c7ddb9c66a756043775b2f6ce1
dnhckt <hacketta.t@gmail.com>
2017-07-20 23:25:55 +0100

    All questions added. Fullscreen mode enabled

--- GOAT.py
+++ GOAT.py
@@ -11,7 +11,7 @@ import GOATquestions
     
 """
 
-# SKILL VARIABLES ------------------------------
+# SKILL VARIABLES START ------------------------------
     
 # Combat
 unarmed = 0
@@ -32,15 +32,137 @@ barter = 0
 
 # SKILL VARIABLES END -----------------------------
 
-startWin = gui("Generalized Occupational Aptitude Test") # Initial window
-question = gui("Generalized Occupational Aptitude Test") # Question winow
+# SET WINDOWS START  ----------------------------------------------------------
+
+startWin = gui("Generalized Occupational Aptitude Test") 
+question1 = gui("Generalized Occupational Aptitude Test")
+question2 = gui("Generalized Occupational Aptitude Test")
+question3 = gui("Generalized Occupational Aptitude Test")
+question4 = gui("Generalized Occupational Aptitude Test")
+question5 = gui("Generalized Occupational Aptitude Test")
+question6 = gui("Generalized Occupational Aptitude Test")
+question7 = gui("Generalized Occupational Aptitude Test")
+question8 = gui("Generalized Occupational Aptitude Test")
+question9 = gui("Generalized Occupational Aptitude Test")
+question10 = gui("Generalized Occupational Aptitude Test")
+
+startWin.setGeometry("fullscreen")
+question1.setGeometry("fullscreen")
+question2.setGeometry("fullscreen")
+question3.setGeometry("fullscreen")
+question4.setGeometry("fullscreen")
+question5.setGeometry("fullscreen")
+question6.setGeometry("fullscreen")
+question7.setGeometry("fullscreen")
+question8.setGeometry("fullscreen")
+question9.setGeometry("fullscreen")
+question10.setGeometry("fullscreen")
+
+
+
 qCount = 0 # Used to display different question labels
+
+# SET WINDOWS END -------------------------------------------------
 
 # FUNCTIONS START  -----------------------------------------------
 
+
+def setup(q):
+    q.setBg("green")
+    q.addLabel("qNum", "Question " + str(qCount))
+    q.addLabel("Question", "")
+
+
+def qRep(count, q):
+
+        q.setLabel("qNum", "Question " + str(qCount))
+        
+        # Question One                                 
+        if count == 1:
+            q.setLabel("Question", GOATquestions.qOne)
+                
+            q.addRadioButton("ans", GOATquestions.qOneA)
+            q.addRadioButton("ans", GOATquestions.qOneB)
+            q.addRadioButton("ans", GOATquestions.qOneC)
+            q.addRadioButton("ans", GOATquestions.qOneD)
+
+        if count == 2:
+            q.setLabel("Question", GOATquestions.qTwo)
+            
+            q.addRadioButton("ans", GOATquestions.qTwoA)
+            q.addRadioButton("ans", GOATquestions.qTwoB)
+            q.addRadioButton("ans", GOATquestions.qTwoC)
+            q.addRadioButton("ans", GOATquestions.qTwoD)
+ 
+          #  question.setRadioButtonChangeFunction("ans", function)   
+        
+        if count == 3:
+            q.setLabel("Question", GOATquestions.qThree)  
+            
+            q.addRadioButton("ans", GOATquestions.qThreeA)
+            q.addRadioButton("ans", GOATquestions.qThreeB)
+            q.addRadioButton("ans", GOATquestions.qThreeC)
+            q.addRadioButton("ans", GOATquestions.qThreeD)
+                     
+        if count == 4:
+            q.setLabel("Question", GOATquestions.qFour)
+            
+            q.addRadioButton("ans", GOATquestions.qFourA)
+            q.addRadioButton("ans", GOATquestions.qFourB)
+            q.addRadioButton("ans", GOATquestions.qFourC)
+            q.addRadioButton("ans", GOATquestions.qFourD)
+            
+        if count == 5:
+            q.setLabel("Question", GOATquestions.qFive)
+            
+            q.addRadioButton("ans", GOATquestions.qFiveA)
+            q.addRadioButton("ans", GOATquestions.qFiveB)
+            q.addRadioButton("ans", GOATquestions.qFiveC)
+            q.addRadioButton("ans", GOATquestions.qFiveD)
+            
+        if count == 6: 
+            q.setLabel("Question", GOATquestions.qSix)
+            
+            q.addRadioButton("ans", GOATquestions.qSixA)
+            q.addRadioButton("ans", GOATquestions.qSixB)
+            q.addRadioButton("ans", GOATquestions.qSixC)
+            q.addRadioButton("ans", GOATquestions.qSixD)
+            
+        if count == 7:
+            q.setLabel("Question", GOATquestions.qSeven)
+            
+            q.addRadioButton("ans", GOATquestions.qSevenA)
+            q.addRadioButton("ans", GOATquestions.qSevenB)
+            q.addRadioButton("ans", GOATquestions.qSevenC)
+            q.addRadioButton("ans", GOATquestions.qSevenD)
+            
+        if count == 8:
+            q.setLabel("Question", GOATquestions.qEight)
+            
+            q.addRadioButton("ans", GOATquestions.qEightA)
+            q.addRadioButton("ans", GOATquestions.qEightB)
+            q.addRadioButton("ans", GOATquestions.qEightC)
+            q.addRadioButton("ans", GOATquestions.qEightD)
+            
+        if count == 9:
+            q.setLabel("Question", GOATquestions.qNine)
+            
+            q.addRadioButton("ans", GOATquestions.qNineA)
+            q.addRadioButton("ans", GOATquestions.qNineB)
+            q.addRadioButton("ans", GOATquestions.qNineC)
+            q.addRadioButton("ans", GOATquestions.qNineD)
+            
+        if count == 10:
+            q.setLabel("Question", GOATquestions.qTen)
+
+            q.addRadioButton("ans", GOATquestions.qTenA)
+            q.addRadioButton("ans", GOATquestions.qTenB)
+            q.addRadioButton("ans", GOATquestions.qTenC)
+            q.addRadioButton("ans", GOATquestions.qTenD)
+
 # Function to check user answers 
 def function(a):
-    print(question.getRadioButton("ans"))
+    print(question1.getRadioButton("ans"))
 """ Note to self : set Changeradiobutton function to check which answer and add to variables accordingly"""
 
 # Function to control buttons
@@ -51,41 +173,97 @@ def startPress(btn):
         global qCount
         qCount += 1
         
+        
         # Initialize window
-        
-        question.setBg("green")
-        question.addLabel("Title", "Question " + str(qCount))
-        question.addLabel("Question", "")
-        
+        setup(question1)
+    
 # CHANGE QUESTIONS ---------------------------------------------
 
-        # Question One                 
-                   
-        if qCount == 1:
-            question.setLabel("Question", GOATquestions.qOne)
-                
-            question.addRadioButton("ans", GOATquestions.qOneA)
-            question.addRadioButton("ans", GOATquestions.qOneB)
-            question.addRadioButton("ans", GOATquestions.qOneC)
-            question.addRadioButton("ans", GOATquestions.qOneD)
- 
-            question.setRadioButtonChangeFunction("ans", function)
-            
+        qRep(qCount, question1)            
+                                 
 # CHANGE QUESTIONS END ---------------------------------------------
- 
-        question.addButton("Continue", startPress)
-        question.go()
-   
+
+        question1.addButton("Continue", startPress)
+        question1.go()
+        
     if btn == "Quit":
-        question.stop()
+        question1.stop()
+        question2.stop()
+        question3.stop()
+        question4.stop()
+        question5.stop()
+        question6.stop()
+        question7.stop()
+        question8.stop()
+        question9.stop()
+        question10.stop()
         startWin.stop()
         
     if btn == "Continue":
-        question.stop()   
+        qCount += 1
+        if qCount == 2:
+            question1.stop()
+            setup(question2)     
+            qRep(qCount, question2) 
+            question2.addButton("Continue", startPress)
+            question2.go()
+        if qCount == 3:
+            question2.stop()
+            setup(question3)     
+            qRep(qCount, question3) 
+            question3.addButton("Continue", startPress)
+            question3.go()
+        if qCount == 4:
+            question3.stop()
+            setup(question4)     
+            qRep(qCount, question4) 
+            question4.addButton("Continue", startPress)
+            question4.go()
+        if qCount == 5:
+            question4.stop()
+            setup(question5)     
+            qRep(qCount, question5) 
+            question5.addButton("Continue", startPress)
+            question5.go()
+        if qCount == 6:
+            question5.stop()
+            setup(question6)     
+            qRep(qCount, question6) 
+            question6.addButton("Continue", startPress)
+            question6.go()
+        if qCount == 7:
+            question6.stop()
+            setup(question7)     
+            qRep(qCount, question7) 
+            question7.addButton("Continue", startPress)
+            question7.go()
+        if qCount == 8:
+            question7.stop()
+            setup(question8)     
+            qRep(qCount, question8) 
+            question8.addButton("Continue", startPress)
+            question8.go()
+        if qCount == 9:
+            question8.stop()
+            setup(question9)     
+            qRep(qCount, question9) 
+            question9.addButton("Continue", startPress)
+            question9.go()
+        if qCount == 10:
+            question9.stop()
+            setup(question10)     
+            qRep(qCount, question10) 
+            question10.addButton("Continue", startPress)
+            question10.go() 
+        if qCount > 10:
+            question10.stop()
+            qCount = 0                 
+            
         
 # FUNCTIONS END ----------------------------------------------------
          
 # Initialize start window
+
 
 startWin.setBg("green")
 startWin.addLabel("welcome", "Welcome to the G.O.A.T")
